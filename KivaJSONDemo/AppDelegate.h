50eb21b6e93f78dc35fcf0eda8f6e9fedff3b33c
Chris Paveglio <chrispav@ptd.net>
2014-06-19 09:52:32 -0400

    *raw new project

---
+++ KivaJSONDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KivaJSONDemo
+//
+//  Created by Chris Paveglio on 6/19/14.
+//  Copyright (c) 2014 nobody. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KivaJSONDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KivaJSONDemo
+//
+//  Created by Chris Paveglio on 6/19/14.
+//  Copyright (c) 2014 nobody. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
